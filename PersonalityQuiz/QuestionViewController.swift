482c7c54a538c01ce16d0b1b8ce5d5b7518130ec
Chris Gantt <gantt.art@gmail.com>
2020-02-05 14:11:40 -0800

    Labels updated for each question

--- PersonalityQuiz/QuestionViewController.swift
+++ PersonalityQuiz/QuestionViewController.swift
@@ -10,13 +10,39 @@ import UIKit
 
 class QuestionViewController: UIViewController {
 
+    @IBOutlet var questionLabel: UILabel!
+    
     @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButton1: UIButton!
+    @IBOutlet var singleButton2: UIButton!
+    @IBOutlet var singleButton3: UIButton!
+    @IBOutlet var singleButton4: UIButton!
+    
     @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multiLabel1: UILabel!
+    @IBOutlet var multiLabel2: UILabel!
+    @IBOutlet var multiLabel3: UILabel!
+    @IBOutlet var multiLabel4: UILabel!
+    
     @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabel1: UILabel!
+    @IBOutlet var rangedLabel2: UILabel!
+    
+    @IBOutlet var questionProgressView: UIProgressView!
     
     var questionIndex = 0
 
     var questions: [Question] = [
+        Question(text: "How much do you enjoy car rides?",
+        type: .ranged,
+        answers: [
+               Answer(text: "I dislike them", type: .cat),
+               Answer(text: "I get a little nervous",
+                     type: .rabbit),
+               Answer(text: "I barely notice them",
+                     type: .turtle),
+               Answer(text: "I love them", type: .dog)
+                    ]),
         Question(text: "Which food do you like the most?",
                  type:.single,
                  answers: [
@@ -34,16 +60,7 @@ class QuestionViewController: UIViewController {
                         Answer(text: "Eating", type: .dog)
                               ]),
         
-        Question(text: "How much do you enjoy car rides?",
-                 type: .ranged,
-                 answers: [
-                        Answer(text: "I dislike them", type: .cat),
-                        Answer(text: "I get a little nervous",
-                              type: .rabbit),
-                        Answer(text: "I barely notice them",
-                              type: .turtle),
-                        Answer(text: "I love them", type: .dog)
-                             ])
+        
         ]
     
     override func viewDidLoad() {
@@ -56,17 +73,47 @@ class QuestionViewController: UIViewController {
         multipleStackView.isHidden = true
         rangedStackView.isHidden = true
         
-        navigationItem.title = "Question #\(questionIndex+1)"
         
         let currentQuestion = questions[questionIndex]
+        let currentAnswers = currentQuestion.answers
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        navigationItem.title = "Question #\(questionIndex+1)"
+        questionLabel.text = currentQuestion.text
+        questionProgressView.setProgress(totalProgress, animated: true)
         
         switch currentQuestion.type {
         case .single:
             singleStackView.isHidden = false
+            updateSingleStack(using: currentAnswers)
         case .multiple:
             multipleStackView.isHidden = false
+            updateMultipleStack(using: currentAnswers)
         case .ranged:
             rangedStackView.isHidden = false
+            updateRangedStack(using: currentAnswers)
             }
+    }
+    
+    func updateSingleStack(using answers: [Answer]) {
+        singleStackView.isHidden = false
+        singleButton1.setTitle(answers[0].text, for: .normal)
+        singleButton2.setTitle(answers[1].text, for: .normal)
+        singleButton3.setTitle(answers[2].text, for: .normal)
+        singleButton4.setTitle(answers[3].text, for: .normal)
+    }
+    
+    func updateMultipleStack(using answers: [Answer]) {
+        multipleStackView.isHidden = false
+        multiLabel1.text = answers[0].text
+        multiLabel2.text = answers[1].text
+        multiLabel3.text = answers[2].text
+        multiLabel4.text = answers[3].text
+    }
+    
+    func updateRangedStack(using answers: [Answer]) {
+        rangedStackView.isHidden = false
+        rangedLabel1.text = answers.first?.text
+        rangedLabel2.text = answers.last?.text
     }
 }
